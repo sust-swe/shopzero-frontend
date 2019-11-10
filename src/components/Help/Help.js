33290d4a919f5434916c732f3ebb590f0883958c
Tushita2016831026 <kazi26@student.sust.edu>
2019-11-10 11:30:24 +0600

    Changed product page

--- src/components/Help/Help.js
+++ src/components/Help/Help.js
@@ -2,14 +2,22 @@ import React from "react";
 import Logo from "../Logo/Logo";
 import classes from "./Help.css";
 import { MDBRow, MDBCol } from "mdbreact";
+import ShowMenu from "../../containers/ShowMenu/ShowMenu";
 
 const Help = () => {
   return (
     <div>
       <div className={classes.Help}>
         <Logo />
-        <h1>Help Page</h1>
       </div>
+      <MDBRow>
+        <MDBCol md="2">
+          <ShowMenu />
+        </MDBCol>
+        <MDBCol className={classes.Help}>
+          <h1>Help Page</h1>
+        </MDBCol>
+      </MDBRow>
     </div>
   );
 };
--- src/containers/Products/ProductPage/ProductPage.js
+++ src/containers/Products/ProductPage/ProductPage.js
@@ -15,10 +15,14 @@ class ProductPage extends Component {
   constructor(props) {
     super(props);
 
-    const info = props.productInfo;
-    const result = Object.keys(info).map(key => info[key]);
+    if (props.productInfo) {
+      const info = props.productInfo;
+      const result = Object.keys(info).map(key => info[key]);
 
-    this.state = { info: result };
+      this.state = { info: result };
+    } else {
+      this.state = { info: null };
+    }
   }
 
   componentDidMount() {
@@ -27,7 +31,7 @@ class ProductPage extends Component {
   }
 
   render() {
-    return (
+    let page = (
       <div>
         <div className={[classes.Row, `row`].join(" ")}>
           <div className="col-md-2">
@@ -50,13 +54,13 @@ class ProductPage extends Component {
               </MDBCol>
               <MDBCol>
                 <MDBCol>
-                  <h3>{this.state.info[1]}</h3>
+                  <h3>name</h3>
                 </MDBCol>
                 <MDBCol>
                   <h3>brand</h3>
                 </MDBCol>
                 <MDBCol>
-                  <h3>{this.state.info[2]}</h3>
+                  <h3>price</h3>
                 </MDBCol>
                 <MDBCol>
                   <h3>qty</h3>
@@ -76,6 +80,60 @@ class ProductPage extends Component {
         </MDBRow>
       </div>
     );
+
+    if (this.state.info) {
+      let page = (
+        <div>
+          <div className={[classes.Row, `row`].join(" ")}>
+            <div className="col-md-2">
+              <Logo />
+            </div>
+            <div className="col-md-8">
+              <SearchBar />
+            </div>
+          </div>
+
+          <MDBRow>
+            <MDBCol md="2">
+              <ShowMenu />
+            </MDBCol>
+
+            <MDBCol md="10" className={classes.Product}>
+              <MDBRow>
+                <MDBCol>
+                  <h3>Image</h3>
+                </MDBCol>
+                <MDBCol>
+                  <MDBCol>
+                    <h3>{this.state.info[1]}</h3>
+                  </MDBCol>
+                  <MDBCol>
+                    <h3>brand</h3>
+                  </MDBCol>
+                  <MDBCol>
+                    <h3>{this.state.info[2]}</h3>
+                  </MDBCol>
+                  <MDBCol>
+                    <h3>qty</h3>
+                  </MDBCol>
+                  <MDBCol>
+                    <h3>cart</h3>
+                  </MDBCol>
+                </MDBCol>
+              </MDBRow>
+              <MDBCol>
+                <h3>features</h3>
+              </MDBCol>
+              <MDBCol>
+                <h3>description</h3>
+              </MDBCol>
+            </MDBCol>
+          </MDBRow>
+        </div>
+      );
+    }
+
+    return page;
   }
 }
 
