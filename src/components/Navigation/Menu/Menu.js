8e1c6b18af1c7977093c25efc3b7d93530f3bcbc
Ishan(2016831017) <ahsan17@student.sust.edu>
2019-12-12 18:00:32 +0600

    Menu Re-Design

--- src/components/Navigation/Menu/Menu.js
+++ src/components/Navigation/Menu/Menu.js
@@ -2,14 +2,14 @@ import React, { Component } from "react";
 import MenuItems from "./MenuItems/MenuItems";
 import classes from "./Menu.css";
 class Menu extends Component {
-  state = {};
-  render() {
-    return (
-      <div className={classes.Menu}>
-        <MenuItems />
-      </div>
-    );
-  }
+	state = {};
+	render() {
+		return (
+			<div className={classes.Menu}>
+				<MenuItems />
+			</div>
+		);
+	}
 }
 
 export default Menu;
--- src/components/Navigation/Menu/MenuItems/MenuItem/MenuItem.js
+++ src/components/Navigation/Menu/MenuItems/MenuItem/MenuItem.js
@@ -3,15 +3,15 @@ import classes from "./MenuItem.css";
 import { NavLink } from "react-router-dom";
 
 const MenuItem = props => (
-  <li className={classes.MenuItem}>
-    <NavLink
-      to={props.link}
-      activeClassName={classes.active}
-      exact={props.exact}
-    >
-      {props.children}
-    </NavLink>
-  </li>
+	<li className={classes.MenuItem}>
+		<NavLink
+			to={props.link}
+			activeClassName={classes.active}
+			exact={props.exact}
+		>
+			{props.children}
+		</NavLink>
+	</li>
 );
 
 export default MenuItem;
